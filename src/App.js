650650a9ca8234d0218af89da3b64652925330da
ifirmawan <firmawaneiwan@gmail.com>
2021-04-22 12:21:16 +0700

    initial commit with eslint and prettier config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Hello World</title>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +23,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
